77d2930bedd3e67d110c4ed2d13c6efb4345ff05
grant <g@hallyb.me>
2024-11-13 13:21:03 +0000

    Added guards

--- src/guards.ts
+++ src/guards.ts
@@ -1,3 +1,10 @@
 import { Maybe, Some, None } from "./types";
 
-// Todo Typeguards for Maybe
+export function Some<T>(x: Maybe<T>): x is T {
+    return x !== undefined && x !== null;
+}
+
+export function None<T>(x: Maybe<T>): x is None {
+    return x === undefined || x === null;
+}
+
